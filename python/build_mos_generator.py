2a7732d0cd0b6ce0b95837f074738e5119a7e784
Karl Benoit <karl.benoit@opal-rt.com>
2026-02-26 18:53:54 -0500

    EP-2876: include non GenUnits to the build

--- python/build_mos_generator.py
+++ python/build_mos_generator.py
@@ -107,11 +107,37 @@ def save_component_list(dest_dir, components):
         json.dump(components, components_json)
 
 
+def prepare_component_list(modelica_root, component_type, whitelist, blacklist):
+    filtered_components = list()
+    path = component_type.split('.')
+    components_dir = pathlib.Path(os.path.join(modelica_root, *path))
+    for component_file in components_dir.glob("*.mo"):
+        component_name = os.path.basename(component_file).strip(".mo")
+        if component_name == "package":
+            continue
+
+        if len(whitelist) and component_name not in whitelist:
+            continue
+
+        if not len(whitelist) and component_name in blacklist:
+            continue
+
+        filtered_components.append(f"{component_type}.{component_name}")
+    return filtered_components
+
+
 if __name__ == "__main__":
+    # Those components are skipped because they don't compile with Dymola and would fail the build. They should be fixed in the future and re-enabled.
+    components_to_skip = [
+        "EX_T1",
+        "MAXEX1",
+        "MAXEX2"
+    ]
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-build-fmu', action='store_true', help='skip build fmu section')
     parser.add_argument('--whitelist', type=str, help='coma separated component list', default="")
-    parser.add_argument('--blacklist', type=str, help='coma separated component list', default="")
+    parser.add_argument('--blacklist', type=str, help='coma separated component list', default=",".join(components_to_skip))
     parser.add_argument('--no-exit-on-error', action='store_true', help='output directory')
     parser.add_argument('--force', action='store_true', help='output directory')
     parser.add_argument('--generate-mos', action='store_true', help='whether to generate a .mos to generate FMUs (openmodelica only)')
@@ -123,36 +149,23 @@ if __name__ == "__main__":
     if not os.path.exists(args.build_dir):
         os.makedirs(args.build_dir)
 
-    component_dir = pathlib.Path(os.path.join(args.modelica_root, 'OpalRT', 'ePHASORSIM', 'WrappedModels'))
     blacklist = list(filter(None, args.blacklist.split(',')))
-
-    component_list = list(filter(None, args.whitelist.split(',')))
+    whitelist = list(filter(None, args.whitelist.split(',')))
     build_dir = pathlib.Path(os.path.join(args.build_dir))
-
-    for mo_file in build_dir.rglob("*.mo"):
-        os.remove(mo_file)
 
     filtered_components = list()
 
-    for component_file in component_dir.rglob("*.mo"):
-        component_name = os.path.basename(component_file).strip(".mo")
-        if component_name == "package":
-            continue
+    component_types = [
+        'OpalRT.Electrical.Control.Excitation',
+        'OpalRT.Electrical.Control.OverExcitationLimiter',
+        'OpalRT.Electrical.Control.UnderExcitationLimiter',
+        'OpalRT.Electrical.Control.Stabilizer',
+        'OpalRT.Electrical.Control.TurbineGovernor',
+        'OpalRT.ePHASORSIM.WrappedModels'
+    ]
 
-        if len(component_list) and component_name not in component_list:
-            continue
-
-        if not len(component_list) and component_name in blacklist:
-            continue
-
-        genunit_dir = os.path.dirname(component_file.as_posix()).replace(f"{component_dir.as_posix()}", "")
-        if genunit_dir.startswith('/Data'):
-            continue
-
-        genunit_dir_tree = ['OpalRT', 'ePHASORSIM', 'WrappedModels']
-        genunit_dir_tree.extend([x for x in genunit_dir.split('/') if x])
-
-        filtered_components.append(f"{'.'.join(genunit_dir_tree)}.{component_name}")
+    for type in component_types:
+        filtered_components.extend(prepare_component_list(args.modelica_root, type, whitelist, blacklist))
 
     # save filtered component list to be used at later stages
     save_component_list(args.build_dir, filtered_components)
